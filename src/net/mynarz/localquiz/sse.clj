e0ce6ac90503e7694c69ca7fe5d681f6d2186f89
Jindrich Mynarz <mynarzjindrich@gmail.com>
2025-10-28 19:44:00 +0100

    Get rid of d*/with-open-sse that close the connection. Add logging.

--- src/net/mynarz/localquiz/sse.clj
+++ src/net/mynarz/localquiz/sse.clj
@@ -7,15 +7,16 @@
             [dev.onionpancakes.chassis.core :as h]
             [starfederation.datastar.clojure.adapter.http-kit :as hk-gen]
             [starfederation.datastar.clojure.api :as d*]
-            [starfederation.datastar.clojure.brotli :as brotli]))
+            [starfederation.datastar.clojure.brotli :as brotli]
+            [taoensso.timbre :as log]))
 
 (defn handler
-  "Server-sent events handler."
-  [{{last-event-id "last-event-id"} :headers
+  "Server-sent events handler that runs `render-fn` for each game update."
+  [render-fn
+   {{last-event-id "last-event-id"} :headers
     {player-game-id :game-id} :path-params
     moderator-game-id :sid
-    :as request}
-   render-fn]
+    :as request}]
   (let [game-id (or player-game-id moderator-game-id)
         <ch (a/sub refresh-pub game-id (a/chan (a/dropping-buffer 1)))
         ;; poison pill for work cancelling
@@ -25,33 +26,37 @@
 
                             hk-gen/on-open
                             (fn [sse-gen]
-                              (d*/with-open-sse sse-gen
-                                (thread
-                                  (loop [last-view-hash last-event-id]
-                                    (a/alt!!
-                                      [<cancel]
-                                      (do (a/close! <ch)
-                                          (a/close! <cancel))
+                              (log/infof "Opening a connection to game %s." game-id)
+                              ; Ensures at least one render on connect
+                              (a/>!! <ch :first-render)
+                              (thread
+                                (loop [last-view-hash last-event-id]
+                                  (a/alt!!
+                                    [<cancel]
+                                    (do (a/close! <ch)
+                                        (a/close! <cancel))
 
-                                      [<ch]
-                                      ([_]
-                                       (some-> ; Stop in case of error
-                                        (on-cpu-pool ; CPU work on real threads
-                                         ; Stop in case of error
-                                         (when-some [new-view (error/try-on-error (render-fn request))]
-                                           (let [new-view-str (h/html new-view)
-                                                 ; This is a very fast hash
-                                                 new-view-hash (Integer/toHexString (hash new-view-str))]
-                                             ; Only send an event if the view has changed
-                                             (when-not (= last-view-hash new-view-hash)
-                                               (d*/patch-elements! sse-gen new-view-str))
-                                             new-view-hash)))
-                                        recur))
+                                    [<ch]
+                                    ([_]
+                                     (log/infof "Rendering game %s." game-id)
+                                     (some-> ; Stop in case of error
+                                      (on-cpu-pool ; CPU work on real threads
+                                       ; Stop in case of error
+                                       (when-some [new-view (error/try-on-error (render-fn request))]
+                                         (let [new-view-str (h/html new-view)
+                                               ; This is a very fast hash
+                                               new-view-hash (Integer/toHexString (hash new-view-str))]
+                                           ; Only send an event if the view has changed
+                                           (when-not (= last-view-hash new-view-hash)
+                                             (d*/patch-elements! sse-gen new-view-str))
+                                           new-view-hash)))
+                                      recur))
 
-                                      ; We want work cancelling to have higher priority.
-                                      :priority true)))))
+                                    ; We want work cancelling to have higher priority.
+                                    :priority true))))
 
                             hk-gen/on-close
                             (fn [sse-gen _]
+                              (log/infof "Closing the connection to game %s." game-id)
                               (a/>!! <cancel :cancel)
                               (d*/close-sse! sse-gen))})))
