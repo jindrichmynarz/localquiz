eb03485cf663fb40765d51c47c92329804160dd5
Jindrich Mynarz <mynarzjindrich@gmail.com>
2025-12-22 16:50:39 +0100

    Improve formatting

--- src/net/mynarz/localquiz/config.clj
+++ src/net/mynarz/localquiz/config.clj
@@ -6,6 +6,8 @@
   :start (let [environment (System/getProperty "app.env")
                is-dev? (= environment "dev")
                {:keys [host-name port] :as config} (read-edn-resource ".config.edn")]
-           (assoc config :url (if is-dev?
-                                (format "http://localhost:%d" port)
-                                (str "https://" host-name)))))
+           (assoc config
+                  :url
+                  (if is-dev?
+                    (format "http://localhost:%d" port)
+                    (str "https://" host-name)))))
