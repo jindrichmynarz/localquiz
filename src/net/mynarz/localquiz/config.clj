f1ef32854e377f616b0670ddd8b8b5e9426395b7
Jindrich Mynarz <mynarzjindrich@gmail.com>
2026-02-19 21:13:11 +0100

    Add default config

--- src/net/mynarz/localquiz/config.clj
+++ src/net/mynarz/localquiz/config.clj
@@ -6,7 +6,8 @@
   :start (let [environment (System/getProperty "app.env")
                is-dev? (= environment "dev")
                {:keys [host-name port]
-                :as config} (read-edn-resource ".config.edn")]
+                :as config} (merge (read-edn-resource "default_config.edn")
+                                   (read-edn-resource ".config.edn"))]
            (assoc config :is-dev? is-dev?
                          :url (if is-dev?
                                 (format "http://localhost:%d" port)
