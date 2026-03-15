5da52ba4cb63435d1fcf3b41dc682741dd4aee93
Jindrich Mynarz <mynarzjindrich@gmail.com>
2026-03-15 16:05:59 +0100

    Start responding middleware

--- src/net/mynarz/localquiz/routes.clj
+++ src/net/mynarz/localquiz/routes.clj
@@ -6,12 +6,14 @@
             [net.mynarz.localquiz.views.common :as views]
             [net.mynarz.localquiz.views.moderator] ; Required to load the moderator-specific methods of views/view.
             [net.mynarz.localquiz.views.player] ; Required to load the player-specific methods of view/view.
-            [reitit.ring.middleware.multipart :as multipart]))
+            [reitit.ring.middleware.multipart :as multipart]
+            [starfederation.datastar.clojure.adapter.http-kit2 :as hk-adapter]))
 
 (def routes
   [; Moderator's routes
    ["/" {:get views/shim-view}]
-   ["/sse" {:get sse/handler}]
+   ["/sse" {:get sse/handler
+            :middleware [hk-adapter/start-responding-middleware]}]
    ["/create"
     ["" {:post {:handler (comp views/view moderator-actions/create-game!)
                 :parameters {:form {:number-of-questions ::s/number-of-questions}
