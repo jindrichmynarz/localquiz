f70016baaeef74b106ca7870a1ea974f0bb568fa
Jindrich Mynarz <mynarzjindrich@gmail.com>
2026-04-03 22:14:47 +0200

    Pass in configuration via arguments

--- src/net/mynarz/localquiz/config.clj
+++ src/net/mynarz/localquiz/config.clj
@@ -1,13 +1,13 @@
 (ns net.mynarz.localquiz.config
   (:require [net.mynarz.localquiz.util :refer [read-edn-resource]]
-            [mount.core :refer [defstate]]))
+            [mount.core :as mount :refer [defstate]]))
 
 (defstate config
   :start (let [environment (System/getProperty "app.env")
                is-dev? (= environment "dev")
                {:keys [host-name port]
                 :as config} (merge (read-edn-resource "default_config.edn")
-                                   (read-edn-resource ".config.edn"))]
+                                   (mount/args))]
            (assoc config :is-dev? is-dev?
                          :url (if is-dev?
                                 (format "http://localhost:%d" port)
--- src/net/mynarz/localquiz/core.clj
+++ src/net/mynarz/localquiz/core.clj
@@ -5,29 +5,67 @@
             [net.mynarz.localquiz.game-sweeper]
             [net.mynarz.localquiz.logging]
             [net.mynarz.localquiz.server]
+            [net.mynarz.localquiz.util :refer [long-str]]
             [clojure.java.browse :refer [browse-url]]
+            [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]]
+            [fast-edn.core :as edn]
             [mount.core :as mount])
   (:import (java.util.concurrent Executors)))
 
-;; Make futures use virtual threads
-(set-agent-send-executor!
- (Executors/newVirtualThreadPerTaskExecutor))
+(defn- error-msg
+  [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (apply long-str errors)))
 
-(set-agent-send-off-executor!
- (Executors/newVirtualThreadPerTaskExecutor))
+(defn- exit
+  "Exit with @status and message `msg`.
+  `status` 0 is OK, `status` 1 indicates error."
+  [^Integer status
+   ^String msg]
+  {:pre [(#{0 1} status)]}
+  (println msg)
+  (System/exit status))
 
-(defn -main
-  [& _]
+(def ^:private die
+  (partial exit 1))
+
+(def ^:private info
+  (partial exit 0))
+
+(def cli-options
+  [["-c" "--config CONFIG" "Path to configuration file in EDN"
+    :parse-fn (comp edn/read-once io/as-file)]
+   ["-h" "--help" "Display help message"]])
+
+(defn main
+  [{:keys [config]
+    :or {config {}}}]
+  ;; Make futures use virtual threads
+  (set-agent-send-executor!
+    (Executors/newVirtualThreadPerTaskExecutor))
+
+  (set-agent-send-off-executor!
+    (Executors/newVirtualThreadPerTaskExecutor))
+
   ; Initialize logging to standard error stream
   (.addShutdownHook (Runtime/getRuntime)
                     (Thread. (fn []
                                (mount/stop)
                                (shutdown-agents))))
-  (mount/start))
+  (mount/start-with-args config))
+
+(defn -main
+  [& args]
+  (let [{{:keys [help]} :options
+         :keys [errors options summary]} (parse-opts args cli-options)]
+    (cond help (info summary)
+          errors (die (error-msg errors))
+          :else (main options))))
 
 (comment
   ; Start the application
-  (-main)
+  (main {})
 
   ; Open the application in the browser
   (browse-url (:url config))
