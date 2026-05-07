b64ab18b5a90bafbb3eb002dcb5b7a207b01ab1b
Jindrich Mynarz <mynarzjindrich@gmail.com>
2026-05-07 21:32:47 +0200

    Default score to 0.0 to avoid failing in case a player doesn't answer

--- src/net/mynarz/localquiz/game.clj
+++ src/net/mynarz/localquiz/game.clj
@@ -163,7 +163,7 @@
   (some->> game-id
            (d/q '[:find (pull ?game [{:game/players [[:player/id :as :player-id]
                                                      [:player/name :as :player-name]
-                                                     {:answer/_player [[:answer/score :as :score]]}
+                                                     {:answer/_player [[:answer/score :default 0.0 :as :score]]}
                                                      [:player/score :default 0.0 :as :total-score]]}]) .
                   :in $ ?game-id
                   :where [?game :game/id ?game-id]]
