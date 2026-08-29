d9bf7930c2df2a6907e08171fe40a7f74913f2fb
Jindrich Mynarz <mynarzjindrich@gmail.com>
2026-08-29 17:46:29 +0200

    Remove moved functions

--- test/src/net/mynarz/localquiz/game_test.clj
+++ test/src/net/mynarz/localquiz/game_test.clj
@@ -12,23 +12,6 @@
          5 true?
          50 false?)))
 
-(def options
-  [{:label "Žižkov" :description "Prague 3"}
-   {:label "Vinohrady"}
-   {:text "Karlín"}])
-
-(deftest search-options
-  (are [fragment labels] (= (map game/option-label (game/search-options options fragment))
-                            labels)
-       "vino"   ["Vinohrady"]
-       "ZIZ"    ["Žižkov"]      ; Case and diacritics are normalized away
-       "zizkov" ["Žižkov"]
-       "karlin" ["Karlín"]      ; A :text option is labelled by its :text
-       "r"      ["Vinohrady" "Karlín"]
-       "bork"   []
-       "  "     []
-       nil      []))
-
 (deftest parse-answer
   (are [answer parsed-answer] (= (game/parse-answer answer) parsed-answer)
        "true" true
